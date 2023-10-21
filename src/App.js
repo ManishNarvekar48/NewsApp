b7f286c852832db01af46ac6e64db4914976c1e7
manishnarvekar48@gmail.com <manishnarvekar48@gmail.com>
2023-10-21 14:51:19 +0530

    NewsApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import "./App.css";
 
-function App() {
+
+const App = () => {
+
+  const [ive, setLive] = useState([]);
+  const [search, setSearch] = useState('mumbai')
+
+  const fetchApi = async () => {
+    const url = `https://newsapi.org/v2/everything?q=${search}&apiKey=20ac519b85914119b95aa427644f6ed5`;
+
+    const response = await fetch(url);
+    const resjson = await response.json();
+    setLive(resjson.articles);
+
+  }
+
+  useEffect(() => {
+    fetchApi();
+  }, [search])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='nav'>
+        <h1 style={{ color: 'white' }}>News <span style={{ color: 'yellow' }}>Live</span></h1>
+        <input type='search' onChange={(event) => {
+          setSearch(event.target.value)
+        }} placeholder='Search...'></input>
+      </div>
+
+
+      {!ive ? (
+        <p>no data found</p>
+      ) : (
+        <>
+          {ive.map((e) => {
+            return (
+              <>
+                <div className='cot'>
+                  <div className="card multi" key={e.id}>
+                    <img src={e.urlToImage} className="card-img-top" alt="..." />
+                    <div className="card-body cards">
+                      <h6 className="card-title">{e.title}</h6>
+                      <p className="card-text"> <b>Author :- </b>{e.author}</p>
+                      <a href={e.url} class="btn btn-primary">More...</a>
+                    </div>
+                  </div>
+                </div>
+
+
+              </>
+            );
+          })}
+        </>
+
+
+      )
+
+      }
+
+
+    </>
+  )
+
 }
 
-export default App;
+export default App
